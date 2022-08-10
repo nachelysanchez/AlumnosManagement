964bb19ece8c972922d2f188457759d0788ec0f6
nachelysanchez <nashely-5@hotmail.com>
2022-08-09 21:33:04 -0400

    Creando clase de Conexión

---
+++ Models/Connection.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlumnosManagement.Models
+{
+    class Connection
+    {
+        public string servidor, db;
+        public bool security;
+        public string cadena;
+
+        public void Conectar()
+        {
+            servidor = "DESKTOP-K8OJCDL\\SQLEXPRESS";
+            db = "DB_Sucarnet";
+            security = true;
+            cadena = "Server = " + servidor + "; Initial Catalog = " + db + ";Integrated Security = " + security;
+        }
+    }
+}
